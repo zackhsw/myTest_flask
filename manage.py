b99824e4c2778302b7389b13a743bb9bce155c9f
ZackWong <1073219036@qq.com>
2019-04-01 10:15:51 +0800

    mytest flask --jenkins

--- manage.py
+++ manage.py
@@ -1,12 +1,12 @@
 from app import app
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, SignatureExpired, BadSignature
 if __name__ == '__main__':
-    user_id = 33
+    user_id = 38
     SECRET_KEY = 'hard to get string'
     # Serializer(current_app.config['SECRET_KEY'], expires_in=expiration).dumps({'user_id': user_id})
     s = Serializer(SECRET_KEY, expires_in=604800)
     t = s.dumps({'user_id': user_id})
     token = t.decode('ascii')
-    print(token)
+    print(token,'添加改变----')
     print('token--+-->', token,'<--+--')
     app.run()
