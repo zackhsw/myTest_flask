b1b90d6043fe528069e35e13ca4209602a3f61b4
ZackWong <1073219036@qq.com>
2019-04-02 16:14:58 +0800

    mytest flask --gunicorn test

--- manage.py
+++ manage.py
@@ -2,17 +2,17 @@ from app import app
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, SignatureExpired, BadSignature
 
 if __name__ == '__main__':
-    user_id = 38
-    SECRET_KEY = 'hard to get string'
-    # Serializer(current_app.config['SECRET_KEY'], expires_in=expiration).dumps({'user_id': user_id})
-    s = Serializer(SECRET_KEY, expires_in=604800)
-    t = s.dumps({'user_id': user_id})
-    token = t.decode('ascii')
-    print('push v7')
-    print('push v8')
-    print('push v9')
-    print('push v17')
-    print('push v18')
-    print(token, '添加改变----')
-    print('token--+-->', token, '<--+--')
+    # user_id = 38
+    # SECRET_KEY = 'hard to get string'
+    # # Serializer(current_app.config['SECRET_KEY'], expires_in=expiration).dumps({'user_id': user_id})
+    # s = Serializer(SECRET_KEY, expires_in=604800)
+    # t = s.dumps({'user_id': user_id})
+    # token = t.decode('ascii')
+    # print('push v7')
+    # print('push v8')
+    # print('push v9')
+    # print('push v17')
+    # print('push v18')
+    # print(token, '添加改变----')
+    # print('token--+-->', token, '<--+--')
     app.run(host='0.0.0.0', port=5000)
